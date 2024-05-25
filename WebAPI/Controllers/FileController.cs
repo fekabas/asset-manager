e984ac2660c4b057f4ba457589204deaa1f85475
Fer Kabas <fernandokabas@gmail.com>
2024-05-25 21:10:00 +0200

    Add basic file upload

--- WebAPI/Controllers/FileController.cs
+++ WebAPI/Controllers/FileController.cs
@@ -6,8 +6,7 @@ namespace WebAPI.Controllers;
 [ApiController]
 public class FileController : Controller
 {
-    public FileController(){
-
+    public FileController() {
     }
 
     /// <summary>
@@ -16,12 +15,27 @@ public class FileController : Controller
     /// <param name="file">Form input of type file</param>
     /// <returns>The UUID of the generated file entry</returns>
     [HttpPost]
-    public async Task<IActionResult> Upload([FromBody] IFormFile file){
-        // Parse input
-        // Retrieve metada
+    public async Task<IActionResult> Upload(IFormFile file){
+        // TODO: Parse input
+    
+        // TODO: Check if file already exists
         // Save file to memory
-        // Insert metadata into DB with the fiel path data
-        // Return UUID of metadata
+        // TODO: Make directory configurable
+        string uploads = Path.Combine("./", "VolumeStorage");
+        string filePath = Path.Combine(uploads, file.FileName);
+        using Stream fileStream = new FileStream(filePath, FileMode.Create);
+        await file.CopyToAsync(fileStream);
+
+        // Retrieve metada
+        var type = file.ContentType;
+        var length = file.Length;
+        var name = file.FileName;
+        var createdDate = DateTime.UtcNow;
+        var path = filePath;
+
+        // TODO: Insert metadata into DB with the field path data
+        // TODO: Return UUID of metadata
+
         return Ok();
     }
 
